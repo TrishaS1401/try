934a87fa93d5d5758d09ead6378d6b580b2b887a
TrishaS1401 <56110148+TrishaS1401@users.noreply.github.com>
2021-10-29 19:03:25 +0530

    Add files via upload

---
+++ pracs.py
@@ -0,0 +1,30 @@
+# Python3 Program to compute sum of
+# 1 + x / 2 ! + x ^ 2 / 3 ! +...+x ^ n/(n + 1)!
+
+# Method to find the factorial of a number
+def fact(n):
+	if n == 1:
+		return 1
+	else:
+		return n * fact(n - 1)
+
+# Method to compute the sum
+def sum(x, n):
+	total = 1.0
+	for i in range (1, n + 1, 1):
+		total = total + (pow(x, i) / fact(i))
+
+	return total
+
+
+if __name__== '__main__':
+	
+	# Get x and n
+	x = 5
+	n = 4
+
+	# Print output
+	print ("Sum is: {0:.4f}".format(sum(x, n)))
+	
+
+
